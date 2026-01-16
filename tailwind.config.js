ab905c2e00290c7c9347fca4f703d10dadc277ca
syafiqhadzir <syafiqhadzir@live.com.my>
2026-01-16 22:51:57 +0800

    Fix CSS not reflecting: Setup Tailwind CSS build pipeline with PostCSS and Sass integration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.{html,md}',
+    './_layouts/**/*.{html,md}',
+    './_posts/**/*.{html,md}',
+    './*.{html,md}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
